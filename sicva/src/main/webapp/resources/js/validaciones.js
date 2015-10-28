9b8cabe00e3efc871db06d6b371ac17268a5dd6d
FERNANDO TORRES <fernandotorrescova@hotmail.com>
2015-10-27 18:53:05 -0600

    Validación
    Validación de Dirección

--- sicva/src/main/webapp/resources/js/validaciones.js
+++ sicva/src/main/webapp/resources/js/validaciones.js
@@ -2,7 +2,7 @@ function soloLetras(e) {
 	tecla = (document.all) ? e.keyCode : e.which;
 	if (tecla == 8)
 		return true;
-	patron = /[A-Za-zñÑ-áéíóúÁÉÍÓÚ\s]/;
+	patron = /[A-Za-zñÑ-áéíóúÁÉÍÓÚ-,\s]/;
 	te = String.fromCharCode(tecla);
 	return patron.test(te);
 
@@ -11,7 +11,7 @@ function numYletras(e) {
 	tecla = (document.all) ? e.keyCode : e.which;
 	if (tecla == 8)
 		return true;
-	patron = /[nÑ\s|\w]/;
+	patron = /[nÑ-áéíóúÁÉÍÓÚ\s|\w]/;
 	te = String.fromCharCode(tecla);
 	return patron.test(te);
 
@@ -23,4 +23,13 @@ function numeros(e) {
     patron = /[1234567890]/;  
     te = String.fromCharCode(tecla);
     return patron.test(te); 
+}
+function direccion(e) {
+	tecla = (document.all) ? e.keyCode : e.which;
+	if (tecla == 8)
+		return true;
+	patron = /[nÑ-áéíóúÁÉÍÓÚ\s|#\s|\w]/;
+	te = String.fromCharCode(tecla);
+	return patron.test(te);
+
 }
